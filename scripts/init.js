0a40905f6d27d856e408599fae7c0b7f122e161c
JBGeum <gysophila001@gmail.com>
2024-05-16 17:40:39 +0900

    잡담 관련 편의 추가

--- scripts/init.js
+++ scripts/init.js
@@ -1,39 +1,51 @@
 Hooks.once("setup", function () {
   const commands = game.chatCommands;
   Hooks.on("renderChatMessage", (message, html, messageData) => {
-    if(message.flags.priv_talk){
+    const privFlag = message.flags.priv_talk || message.getFlag('sch-customize', 'priv_talk');
+    if(privFlag){
       html.addClass('priv_talk');
+      html.addClass(`user-${message.user.id}`);
+      if(typeof privFlag === 'string')
+        html.addClass(privFlag);
       html.find('header').css("display","none");
-      html.find('.message-content').prepend(`<div class="priv_user">${message.speaker.alias}</div> `);
+      html.find('.message-content').html(`<div class="pt priv_user">${message.speaker.alias}</div> <div class="pt">${message.content}</div>`);
       if (!game.settings.get("sch-customize", "privTalkSpeakerLineChange"))
         html.addClass('line-change');
     }
   });
+
   commands.register({
     name: "/pt",
     module: "core",
-    aliases: ["/p","`", "!"],
+    aliases: [`${game.settings.get('sch-customize', 'customPrivTalkAlias')}`,"`", "!"],
     icon: "<i class='fas fa-dice-d20'></i>",
     requiredRole: "NONE",
     callback: async function (chat, parameters, messageData){
-      if(!game.actors.getName("잡담")){
-        let actor = await Actor.create({
-          name: "잡담",
-          type: "character"
-        });
-      }
       if (!game.settings.get("sch-customize", "markdownDelUse"))
         parameters = parameters.replace(/<\s*\/?\s*del\s*>/g, '~');
-      messageData.speaker.actor = game.actors.getName("잡담").id;
-      messageData.speaker.token = null;
 
       const speakUser = (messageData.user instanceof User ? messageData.user : game.users.get(messageData.user));
+      messageData.speaker.actor = speakUser.id;
+      messageData.speaker.token = null;
       messageData.speaker.alias = speakUser.name;
+      messageData.type = game.settings.get("sch-customize", "privTalkAsOOC") ? CONST.CHAT_MESSAGE_TYPES.OOC : CONST.CHAT_MESSAGE_TYPES.OTHER;
+      const lastMsg = game.messages.contents[game.messages.size - 1];
+      const lastMgsPrivFlags = lastMsg.getFlag('sch-customize', 'priv_talk');
+      let position = true;
+      if(lastMgsPrivFlags === true) {
+        await game.messages.get(lastMsg.id).setFlag('sch-customize', 'priv_talk', 'top');
+        position = 'end';
+      }else if(lastMgsPrivFlags === 'end'){
+        await game.messages.get(lastMsg.id).setFlag('sch-customize', 'priv_talk', 'middle');
+        position = 'end';
+      }
+
       return {
         content: parameters,
-        // user: game.users.getName("잡담"),
-        // flavor: speakUser.name,
-        flags: {'priv_talk': true}
+        flags: {
+          'sch-customize':
+              {'priv_talk': position}
+        }
       }
     },
     autocompleteCallback: (menu, alias, parameters) => [game.chatCommands.createInfoElement("잡담")],
@@ -42,25 +54,6 @@ Hooks.once("setup", function () {
 });
 
 Hooks.once('init', () => {
-  game.settings.register("sch-customize", "privTalkSpeakerLineChange", {
-    name: "이름 뒤 줄바꿈",
-    hint: "플레이어 닉네임 표시 후 줄바꿈 후 잡담 내용을 표시합니다.",
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-    onChange: _ => window.location.reload()
-  });
-
-  game.settings.register("sch-customize", "markdownDelUse", {
-    name: "채팅에 마크다운 사용 시 취소선 적용 여부",
-    hint: "채팅에 마크다운을 적용 시 ~ 사이에 감싸인 문자를 취소선으로 표시합니다.",
-    scope: "world",
-    config: true,
-    default: false,
-    type: Boolean,
-    onChange: _ => window.location.reload()
-  });
 
   game.settings.registerMenu("sch-customize", "downloadChatArchiveMenu", {
     name: `채팅 로그 다운로드`,
@@ -87,7 +80,7 @@ Hooks.once('init', () => {
   game.settings.register("sch-customize", "hideWhisper", {
     name: "채팅로그 귓속말 숨김",
     hint: "채팅 로그의 귓속말을 가립니다. 회색 배경으로 표시되며 클릭 시 텍스트가 표시됩니다.",
-    scope: "world",
+    scope: "client",
     config: true,
     default: false,
     type: Boolean,
@@ -101,10 +94,167 @@ Hooks.once('init', () => {
     filePicker: "any",
     default: "",
     onChange: value => {
-      if(value.endsWith(".json"))
+      if (value.endsWith(".json"))
         getDFchatArchive(value);
-      else if(value.length > 0)
+      else if (value.length > 0)
         alert("json 파일을 선택해 주세요.")
     }
   });
+
+  game.settings.register("sch-customize", "customPrivTalkAlias", {
+    name: "잡담 시작을 위한 문자 지정",
+    hint: "잡담으로 발언할 때 처음으로 타이핑 하는 커스텀 문자 등록. 기본은 '/pt', '!', '`'로 사용가능합니다.",
+    scope: "client",
+    config: true,
+    default: "/p",
+    type: String,
+    onChange: _ => window.location.reload()
+  });
+
+
+  game.settings.register("sch-customize", "markdownDelUse", {
+    name: "채팅에 마크다운 사용 시 취소선 적용 여부",
+    hint: "채팅에 마크다운을 적용 시 ~ 사이에 감싸인 문자를 취소선으로 표시합니다.",
+    scope: "client",
+    config: true,
+    default: false,
+    type: Boolean,
+    onChange: _ => window.location.reload()
+  });
+
+  game.settings.register("sch-customize", "privTalkAsOOC", {
+    name: "잡담을 OOC로 생성",
+    hint: "이후 생성되는 OOC로 분류됩니다. 기본은 기타(Other)입니다.",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("sch-customize", "privTalkSpeakerLineChange", {
+    name: "이름 뒤 줄바꿈",
+    hint: "플레이어 닉네임 표시 후 줄바꿈 후 잡담 내용을 표시합니다.",
+    scope: "client",
+    config: true,
+    default: false,
+    type: Boolean,
+    onChange: _ => window.location.reload()
+  });
+
+  game.settings.register("sch-customize", "setChatLogFontSize", {
+    name: '기본 채팅 글자 크기',
+    hint: '기본 채팅 글자 크기 조절(기본 14px)',
+    config: true,
+    type: Number,
+    scope: 'client',
+    range: {
+      min: 14,
+      max: 30,
+      step: 0.5
+    },
+    default: 14,
+    onChange: (value) => this.updateCssProperty('clFontSize', `${value}px`)
+  });
+
+
+  game.settings.register("sch-customize", "setPrivTalkFontSize", {
+    name: '사담 글자 크기',
+    hint: '사담 글자 크기 조절(기본 12px)',
+    config: true,
+    type: Number,
+    scope: 'client',
+    range: {
+      min: 10,
+      max: 30,
+      step: 0.5
+    },
+    default: 12,
+    onChange: (value) => this.updateCssProperty('ptFontSize', `${value}px`)
+  });
+
+  game.settings.register("sch-customize", "setPrivTalkFontOpacity", {
+    name: '사담 글자 색상',
+    hint: '사담 글자 진하기 조절(기본 0.8)',
+    config: true,
+    type: Number,
+    scope: 'client',
+    range: {
+      min: 0,
+      max: 1,
+      step: 0.05
+    },
+    default: 0.8,
+    onChange: (value) => this.updateCssProperty('fontColor', `rgba(0,0,0,${value})`)
+  });
+
+  game.settings.register("sch-customize", "setPrivTalkMarginLeft", {
+    name: '사담 여백 간격',
+    hint: '사담 여백 간격 조절(기본 10)',
+    config: true,
+    type: Number,
+    scope: 'client',
+    range: {
+      min: 0,
+      max: 40,
+      step: 1
+    },
+    default: 10,
+    onChange: (value) => this.updateCssProperty('marginLeft', `${value}px`)
+  });
+
+  game.settings.register("sch-customize", "setPrivTalkBgBrightness", {
+    name: '사담 밝기 조절',
+    hint: '사담 배경색을 밝게(기본:0.1)',
+    config: true,
+    type: Number,
+    scope: 'client',
+    range: {
+      min: 0,
+      max: 1,
+      step: 0.05
+    },
+    default: 0.1,
+    onChange: (value) => this.updateCssProperty('brightness', `${value}`)
+  });
+
+
+  this.updateCssProperty('fontColor', `rgba(0,0,0,${(game.settings.get("sch-customize", "setPrivTalkFontOpacity"))})` );
+  this.updateCssProperty('clFontSize',`${game.settings.get("sch-customize", "setChatLogFontSize")}px`);
+  this.updateCssProperty('ptFontSize',`${game.settings.get("sch-customize", "setPrivTalkFontSize")}px`);
+  this.updateCssProperty('marginLeft',`${game.settings.get("sch-customize", "setPrivTalkMarginLeft")}px`);
+  this.updateCssProperty('brightness',`${game.settings.get("sch-customize", "setPrivTalkBgBrightness")}`);
 });
+
+Hooks.once('ready', () => this.setUserColorBg());
+
+const cssProperty = {
+  ptFontSize : '--priv-talk-font-size',
+  fontColor : '--priv-talk-font-color',
+  marginLeft : '--priv-talk-margin-left',
+  brightness : '--priv-talk-bg-brightness',
+  clFontSize : '--sch-cus-chat-font-size'
+}
+
+
+function updateCssProperty(property,value){
+  if(value)
+    document.querySelector(':root').style.setProperty(cssProperty[property], value);
+}
+
+
+function setUserColorBg(){
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  let cssText = '';
+
+  game.users.forEach(user => {
+    cssText += `.user-${user.id} { background: ${user.color}; }`
+  });
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = cssText;
+  } else {
+    style.appendChild(document.createTextNode(cssText));
+  }
+  document.head.appendChild(style);
+}
